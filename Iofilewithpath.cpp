286c352af71b2e79f8e8fee96258e27ee6bb5b50
LENOVO <LENOVO@LAPTOP-56DHCMP1>
2024-06-03 00:44:04 -0700

    menambahkan loop

--- Iofilewithpath.cpp
+++ Iofilewithpath.cpp
@@ -12,4 +12,13 @@ int main(){
 
     // membuka file dalam mode menulis
     ofstream outfile;
+    // menunjuk kesebuah nama file
+    outfile.open(NamaFile + ".txt", ios::out);
+
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+    // unlimited loop untuk menulis
+    while (true){
+        
+    }
 }
