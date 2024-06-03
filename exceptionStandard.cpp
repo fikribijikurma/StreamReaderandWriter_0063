c2edec06b2f051adca546d4286b9bbafaffc4e5b
LENOVO <LENOVO@LAPTOP-56DHCMP1>
2024-06-03 01:09:28 -0700

    menambahkan fungtion pada int main

---
+++ exceptionStandard.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <exeption>
+#include <array>
+using namespace std;
+ 
+ int main()
+ {
+    
+ }
--- manualofException.cpp
+++ manualofException.cpp
@@ -4,6 +4,18 @@ using namespace std;
 int main()
 {
     try {
-        
+        cout << "Selamat Belajar di prodi TI UMY" << endl;
+        // throw 0.5; // melemparkan sebuah interger maka
+        cout << "Pernyataan tidak akan dieksekusi" << endl;
     }
+    catch {
+        // blok ini akan dieksekusi
+        cout << "Pengecualian akan dieksekusi" << endl;
+    }
+    catch {
+        /*jika selain interger maka block ini akan dieksekusi*/
+        cout << "default pengecualian dieksekusi" << endl;
+    }
+
+    return 0;
 }
